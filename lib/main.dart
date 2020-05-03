21ffc2893bb4f70af973f77e1fde5a42b30faee1
Daniel <daniel.pe.15y15@gmail.com>
2020-05-03 19:04:35 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: PikachuApp(),
+));
+
+
+class PikachuApp extends StatefulWidget {
+  @override
+  _PikachuAppState createState() => _PikachuAppState();
+}
+
+class _PikachuAppState extends State<PikachuApp> {
+
+
+  int _nijaLevel = 0;
+
+  String squadMaisLindo = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+       title: Text('App do pikachu'), 
+       centerTitle: true,
+       backgroundColor: Colors.grey[850],
+       elevation: 0.0,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+         CircularButton(
+           height: 60,
+           width: 60,
+           color: Colors.green,
+           icon: Icon(
+             Icons.add
+           ),
+           onClick: () {
+             setState(() {
+               _nijaLevel++;
+             });
+           },
+         ),
+         CircularButton(
+           height: 60,
+           width: 60,
+           color: Colors.red,
+           icon: Icon(
+             Icons.remove
+           ),
+           onClick: () {
+             setState(() {
+               _nijaLevel--;
+             });
+           },
+         ),
+         FlatButton(
+           onPressed: () {
+             setState(() {
+               if ( squadMaisLindo == 'Thunder shock'){
+                 squadMaisLindo = '';
+               } else {
+               squadMaisLindo = 'Thunder shock';
+               }
+             });
+           }, 
+           color: Colors.amber,
+           child: Text('Special attack'),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center( 
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/pikachu.jpeg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Pikachu',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'CURRENT PIKACHU LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              '$_nijaLevel',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'botdopikachu@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  '$squadMaisLindo',
+                  style: TextStyle(
+                    color: Colors.amber[700],
+                    fontSize: 30,
+                  ),
+                ),
+              ],
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+
+class CircularButton extends StatelessWidget {
+
+  final double width;
+  final double height;
+  final Color color;
+  final Icon icon;
+  final Function onClick;
+
+  CircularButton({ this.color, this.height, this.icon, this.onClick, this.width });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+      width: width,
+      height: height,
+      child: IconButton(icon: icon, enableFeedback: true, onPressed: onClick),
+    );
+  }
+}
